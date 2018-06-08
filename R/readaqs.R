6fc8a575b35abac4f6d2d236b0e58b8461aef028
Lisa Oshita <l.oshita@yahoo.com>
2018-06-08 15:16:21 -0700

    add functionality for RC data

--- R/readaqs.R
+++ R/readaqs.R
@@ -2,6 +2,7 @@
 # read.aqs function
 # ==================
 
+# use: devtools::document() to update documentation
 
 #' Read in and format raw data transactions from Air Quality Systems
 #'
@@ -37,7 +38,8 @@
 #'                 \item \strong{Level 4} converts level 3 data to wide format with
 #'                       Date.Time and named monitor IDs as the columns, and Sample.Value
 #'                       as the values in each row.
-#'    }
+#'              }
+#'
 #' @param time.zone A character string specifying the time zone to be used when
 #'              date values are converted to POSIXct class in level 1. Default
 #'              is set to UTC. See as.POSIXct documentation for more time zone options.
@@ -46,8 +48,16 @@
 #'              single value should be dropped. Default is set to FALSE. If TRUE, this
 #'              option will be applied at all levels.
 #'
+#' @param RC A character string indicating if rows corresponding to RC should be dropped or
+#'              returned. Can take either "ignore" (the default) or "include". If set to
+#'              ignore, all rows corresponding to RC will be dropped. If set to "include",
+#'              a list will be returned for each level containing a data frame for RD transactions
+#'              in the first index, and a data frame of RC transactions in the second index.
 #'
-#' @return Returns a data frame.
+#' @param ... Other parameters that will be passed to read.table.
+#'
+#'
+#' @return Returns either a single data frame or a list of two data frames.
 #'
 #'
 #' @examples
@@ -59,47 +69,72 @@
 #' @export
 
 
-read.aqs <- function(file,
-                     level = 2,
-                     time.zone = "UTC",
-                     remove = FALSE) {
+read.aqs <- function(file, level = 2, time.zone = "UTC", remove = FALSE, RC = "ignore", ...) {
+
+  names <- c("RD", "Action.Code", "State.Code", "County.Code", "Site.ID", # column names (for RD only)
+             "Parameter", "POC", "Sample.Duration", "Unit", "Method",
+             "Date", "Start.Time", "Sample.Value", "Null.Data.Code",
+             "Sampling.Frequency", "Monitor.Protocol.MP.ID", "Qualifier - 1",
+             "Qualifier - 2", "Qualifier - 3", "Qualifier - 4", "Qualifier - 5",
+             "Qualifier - 6", "Qualifier - 7", "Qualifier - 8", "Qualifier - 9",
+             "Qualifier - 10", "Alternate.Method.Detectable.Limit", "Uncertainty")
 
   # LEVEL 0 ----------------------------------------
 
   data <- read.table(file = file, # read in data
                      sep = "|",
-                     header = TRUE,
-                     colClasses = c(rep("character", 12),
-                                    "numeric",
-                                    rep("character", 13)))
+                     header = FALSE,
+                     row.names = NULL,
+                     col.names = names,
+                     fill = TRUE,
+                     colClasses = rep("character", 28),
+                     check.names = FALSE,
+                     ...)
 
+  rc.rows <- grep("RC", x = data$RD, value = FALSE) # indexes of RC rows
+
+  if (RC == "include") { # create separate data frame for RC data
+
+    rc.data <- data[rc.rows, ]
+
+    colnames(rc.data) <- c("RD", "Action.Code", "State.Code", "County.Code", "Site.ID", # apply RC column names
+                           "Parameter", "POC", "Unit", "Method", "Year", "Period",
+                           "Number.of.Samples", "Composite.Type", "Sample.Value",
+                           "Monitor.Protocol.(MP).ID", "Qualifier - 1", "Qualifier - 2",
+                           "Qualifier - 3", "Qualifier - 4", "Qualifier - 5", "Qualifier - 6",
+                           "Qualifier - 7", "Qualifier - 8", "Qualifier - 9", "Qualifier - 10",
+                           "Alternate.Method.Detectable.Limit", "Uncertainty")
+
+    rc.data <- rc.data[ , -which(is.na(colnames(rc.data)))]
+    rc.data$Sample.Value <- as.numeric(rc.data$Sample.Value)
+
+  }
+
+  data <- data[-rc.rows, ]
+  data$Sample.Value <- as.numeric(data$Sample.Value)
   state.code <- data$State.Code # used in level 3
 
   if (remove == TRUE) { # if remove = TRUE, drop the columns that contain all the same value
 
     rcol <- rep(0, ncol(data))
-
     for (i in 1:ncol(data)) {
-
       if (length(unique(data[ , i])) == 1) {
-
         rcol[i] <- 1
-
       }
-
     }
-
-    data <- data[ , -which((rcol == 1) & # keep Date column and keep Sample.Value
-                             (colnames(data) != "Date") &
-                             (colnames(data) != "Sample.Value"))]
+    data <- data[ , -which((rcol == 1))]
 
   }
 
   if (level == 0) {
 
     attr(data, "level") <- "level 0"
-
-    return(data)
+    if (RC == "ignore") {
+      return(data)
+    } else if (RC == "include") {
+      attr(rc.data, "level") <- "level 0"
+      return(list(data, rc.data))
+    }
 
   }
 
@@ -119,7 +154,7 @@ read.aqs <- function(file,
                                                   "Action.Code",
                                                   "Null.Data.Code",
                                                   "Sampling.Frequency",
-                                                  "Monitor.Protocol..MP..ID",
+                                                  "Monitor.Protocol.MP.ID",
                                                   "Alternate.Method.Detectable.Limit",
                                                   "Uncertainty"))),
                      (which(startsWith(colnames(data), "Qualifier") == TRUE)))]
@@ -128,8 +163,12 @@ read.aqs <- function(file,
   if (level == 1) {
 
     attr(data, "level") <- "level 1"
-
-    return(data)
+    if (RC == "ignore") {
+      return(data)
+    } else if (RC == "include") {
+      attr(rc.data, "level") <- "level 1"
+      return(list(data, rc.data))
+    }
 
   }
 
@@ -144,10 +183,13 @@ read.aqs <- function(file,
   if (level == 2) {
 
     data <- data[c("Date.Time", "Monitor.ID", "Sample.Value")]
-
     attr(data, "level") <- "level 2"
-
-    return(data)
+    if (RC == "ignore") {
+      return(data)
+    } else if (RC == "include") {
+      attr(rc.data, "level") <- "level 2"
+      return(list(data, rc.data))
+    }
 
   }
 
@@ -181,16 +223,18 @@ read.aqs <- function(file,
                              which(colnames(data)  == "Date.Time"),
                              which(colnames(data)  == "Monitor.ID"))],
                    sep="-")
-
   data$Monitor.ID <- do.call(paste, paste.args1) # concatenate labels
-
   data <- data[, c("Date.Time", "Monitor.ID", "Sample.Value")]
 
   if (level == 3) {
 
     attr(data, "level") <- "level 3"
-
-    return(data)
+    if (RC == "ignore") {
+      return(data)
+    } else if (RC == "include") {
+      attr(rc.data, "level") <- "level 3"
+      return(list(data, rc.data))
+    }
 
   }
 
@@ -206,7 +250,12 @@ read.aqs <- function(file,
   if (level == 4) {
 
     attr(data, "level") <- "level 4"
-    return(data)
+    if (RC == "ignore") {
+      return(data)
+    } else if (RC == "include") {
+      attr(rc.data, "level") <- "level 4"
+      return(list(data, rc.data))
+    }
 
   }
 
@@ -217,4 +266,11 @@ read.aqs <- function(file,
 # - DESCRIPTION (package meta data)
 # - Vignettes (long-form documentation)
 # - Automated testing
+
+
+
+
+
+
+
 
